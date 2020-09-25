2beb7a625e99dfc70b724b371463be949b3fe8dc
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-09-25 14:00:28 +1200

    Added config php file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', 'On'); // change to 'Off' when live
+
+define ("DB_NAME","ayersz70879");
+define ("DB_USERNAME","_ayersz70879");
+define ("DB_PASSWORD","152e5b4d4eb6e5133e416a17ed067e68");
+define("DB_HOST", "localhost");
+
+?>
