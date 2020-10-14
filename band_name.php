86372287985534e3683581406f721aeec7c5d52c
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-10-15 09:44:15 +1300

    Start of simple search area

---
+++ band_name.php
@@ -0,0 +1,32 @@
+<?php include("topbit.php");
+
+    $band_name = $_POST['band_name'];
+
+    $find_sql = "SELECT *
+    FROM `00_L2_games`
+    JOIN 00_L2_games_genre ON (00_L2_games.GenreID = 00_L2_games_genre.GenreID)
+    JOIN 00_L2_games_developer ON (00_L2_games.DeveloperID = 00_L2_games_developer.DeveloperID)
+    WHERE `Name` LIKE '%$name_dev%' OR `DevName` LIKE '%$name_dev%'     
+    
+    ";
+
+    $find_query = mysqli_query($dbconnect, $find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
+    
+
+?>
+
+        <div class="box main">
+            <h2>Name / Developer Results</h2>
+            
+            
+            <?php
+            include("results.php"); 
+            ?>
+            
+
+            
+        </div> <!-- / main -->
+        
+<?php include("bottombit.php")?>
--- bottombit.php
+++ bottombit.php
@@ -2,6 +2,24 @@
     
      <h2>Add an App | <a class="side" href="showall.php">Show All</a></h2>
            
+            <form class="searchform" method="post" action="band_name.php" enctype="multipart/form-data">
+            
+                <input class="search" type="text" name="dev_name" size="40" value="" required placeholder="App Name / Developer Name..."/>
+               
+                <input class="submit" type="submit" name="find_game_name"    value="&#xf002;" />
+               
+            </form>
+            
+            <form class="searchform" method="post" action="free.php" enctype="multipart/form-data">
+            
+                <input class="submit free" type="submit" name="free" value="Free with No In App Purchase &nbsp; &#xf002;" />
+                
+            </form>
+            
+            <br />
+            <hr />
+            <br />
+    
            <h2>Search Area</h2>
            
            <p>This is where the search area goes
