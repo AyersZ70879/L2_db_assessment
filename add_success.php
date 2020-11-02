639153a24f88cc926febb3155e8fc29cc4dae319
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-11-03 12:03:07 +1300

    Create add_success.php

---
+++ add_success.php
@@ -0,0 +1,33 @@
+<?php include("topbit.php");
+
+    // retrieves information...
+    $ID = $_SESSION['ID'];
+
+    $find_sql = "SELECT * FROM `00_L2_bands`
+    JOIN 00_L2_bands_style ON (00_L2_bands.Style1ID = 00_L2_bands_style.StyleID)
+    JOIN 00_L2_bands_style2 ON (00_L2_bands.Style2ID = 00_L2_bands_style.StyleID)
+    JOIN 00_L2_bands_country ON (00_L2_bands.CountryID = 00_L2_bands_country.CountryID)
+    WHERE `ID` = '$ID'
+    ";
+    
+    $find_query = mysqli_query($dbconnect, $find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
+
+?>
+        <div class="box main">
+            <h2>Congratulations</h2>
+            
+            
+            <p>
+               You have added the following entry
+            </p>      
+            
+            <?php
+            include ("results.php");
+            ?>
+
+            
+        </div> <!-- / main -->
+        
+<?php include("bottombit.php")?>
